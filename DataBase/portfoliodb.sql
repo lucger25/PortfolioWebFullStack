17aacbe963636b934d245c77f8709a787146eb1a
Lucas <lcontar18@gmail.com>
2022-04-20 11:06:20 -0300

    Se eliminan controlers innecesarios para integracion

--- DataBase/portfoliodb.sql
+++ DataBase/portfoliodb.sql
@@ -1,207 +1,160 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-04-2022 a las 01:33:18
--- Versión del servidor: 10.4.19-MariaDB
--- Versión de PHP: 8.0.6
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- -----------------------------------------------------
+-- Schema portfoliodb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema portfoliodb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `portfoliodb` DEFAULT CHARACTER SET utf8 ;
+USE `portfoliodb` ;
+
+-- -----------------------------------------------------
+-- Table `portfoliodb`.`persona`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `portfoliodb`.`persona` (
+  `persona_id` MEDIUMTEXT NOT NULL,
+  `persona_user` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `title` VARCHAR(45) NULL,
+  `persona_photo` VARCHAR(150) NULL,
+  `banner_photo` VARCHAR(150) NULL,
+  `birthdate` DATE NULL,
+  `phonenumber` VARCHAR(12) NULL,
+  `mail` VARCHAR(45) NULL,
+  `about_me` VARCHAR(200) NULL,
+  PRIMARY KEY (`persona_id`, `persona_user`))
+ENGINE = InnoDB;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `portfoliodb`.`job_type`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `portfoliodb`.`job_type` (
+  `id` INT NOT NULL,
+  `type` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
---
--- Base de datos: `portfoliodb`
---
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `portfoliodb`.`job_experience`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `portfoliodb`.`job_experience` (
+  `id` INT NOT NULL,
+  `user` VARCHAR(45) NOT NULL,
+  `company_name` VARCHAR(45) NULL,
+  `job_title` VARCHAR(45) NULL,
+  `job_start_date` DATE NULL,
+  `job_end_date` DATE NULL,
+  `job_description` VARCHAR(150) NULL,
+  `job_photo` VARCHAR(200) NULL,
+  `is_current_job` TINYINT NULL,
+  `persona_id` INT NOT NULL,
+  `persona_user` VARCHAR(45) NOT NULL,
+  `job_type_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `persona_id`, `persona_user`, `job_type_id`),
+  CONSTRAINT `fk_job_experience_persona`
+    FOREIGN KEY (`persona_id` , `persona_user`)
+    REFERENCES `portfoliodb`.`persona` (`persona_id` , `persona_user`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_job_experience_job_type1`
+    FOREIGN KEY (`job_type_id`)
+    REFERENCES `portfoliodb`.`job_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
---
--- Estructura de tabla para la tabla `education`
---
 
-CREATE TABLE `education` (
-  `id` int(11) NOT NULL,
-  `user` varchar(45) NOT NULL,
-  `education_title` varchar(100) DEFAULT NULL,
-  `school_name` varchar(100) DEFAULT NULL,
-  `education_start_date` date DEFAULT NULL,
-  `education_end_date` date DEFAULT NULL,
-  `school_photo` varchar(200) DEFAULT NULL,
-  `persona_id` int(11) NOT NULL,
-  `persona_user` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Table `portfoliodb`.`education`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `portfoliodb`.`education` (
+  `id` INT NOT NULL,
+  `user` VARCHAR(45) NOT NULL,
+  `education_title` VARCHAR(100) NULL,
+  `school_name` VARCHAR(100) NULL,
+  `education_start_date` DATE NULL,
+  `education_end_date` DATE NULL,
+  `school_photo` VARCHAR(200) NULL,
+  `persona_id` INT NOT NULL,
+  `persona_user` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`, `persona_id`, `persona_user`),
+  CONSTRAINT `fk_education_persona1`
+    FOREIGN KEY (`persona_id` , `persona_user`)
+    REFERENCES `portfoliodb`.`persona` (`persona_id` , `persona_user`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `job_experience`
---
+-- -----------------------------------------------------
+-- Table `portfoliodb`.`skill`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `portfoliodb`.`skill` (
+  `id` INT NOT NULL,
+  `user` VARCHAR(45) NOT NULL,
+  `skill_title` VARCHAR(45) NULL,
+  `skill_tipe` VARCHAR(45) NULL,
+  `skill_level` VARCHAR(45) NULL,
+  `skill_description` VARCHAR(150) NULL,
+  `persona_id` INT NOT NULL,
+  `persona_user` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`, `persona_id`, `persona_user`),
+  CONSTRAINT `fk_skill_persona1`
+    FOREIGN KEY (`persona_id` , `persona_user`)
+    REFERENCES `portfoliodb`.`persona` (`persona_id` , `persona_user`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE `job_experience` (
-  `id` int(11) NOT NULL,
-  `user` varchar(45) NOT NULL,
-  `company_name` varchar(45) DEFAULT NULL,
-  `job_title` varchar(45) DEFAULT NULL,
-  `job_start_date` date DEFAULT NULL,
-  `job_end_date` date DEFAULT NULL,
-  `job_description` varchar(150) DEFAULT NULL,
-  `job_photo` varchar(200) DEFAULT NULL,
-  `is_current_job` tinyint(4) DEFAULT NULL,
-  `persona_id` int(11) NOT NULL,
-  `persona_user` varchar(45) NOT NULL,
-  `job_type_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `portfoliodb`.`proyect`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `portfoliodb`.`proyect` (
+  `id` INT NOT NULL,
+  `user` VARCHAR(45) NOT NULL,
+  `proyect_title` VARCHAR(45) NULL,
+  `proyect_description` VARCHAR(200) NULL,
+  `proyect_start_date` DATE NULL,
+  `proyect_end_date` DATE NULL,
+  `proyect_photo` VARCHAR(200) NULL,
+  `persona_id` INT NOT NULL,
+  `persona_user` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`, `persona_id`, `persona_user`),
+  CONSTRAINT `fk_proyect_persona1`
+    FOREIGN KEY (`persona_id` , `persona_user`)
+    REFERENCES `portfoliodb`.`persona` (`persona_id` , `persona_user`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
---
--- Estructura de tabla para la tabla `job_type`
---
 
-CREATE TABLE `job_type` (
-  `id` int(11) NOT NULL,
-  `type` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- -----------------------------------------------------
+-- Table `portfoliodb`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `portfoliodb`.`users` (
+  `user_id` MEDIUMTEXT NOT NULL,
+  `user` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `persona_persona_id` MEDIUMTEXT NOT NULL,
+  `persona_persona_user` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`, `persona_persona_id`, `persona_persona_user`),
+  CONSTRAINT `fk_users_persona1`
+    FOREIGN KEY (`persona_persona_id` , `persona_persona_user`)
+    REFERENCES `portfoliodb`.`persona` (`persona_id` , `persona_user`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `persona`
---
-
-CREATE TABLE `persona` (
-  `id` int(11) NOT NULL,
-  `user` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `title` varchar(45) DEFAULT NULL,
-  `persona_photo` varchar(150) DEFAULT NULL,
-  `banner_photo` varchar(150) DEFAULT NULL,
-  `birthdate` date DEFAULT NULL,
-  `phonenumber` varchar(12) DEFAULT NULL,
-  `mail` varchar(45) DEFAULT NULL,
-  `about_me` varchar(200) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `proyect`
---
-
-CREATE TABLE `proyect` (
-  `id` int(11) NOT NULL,
-  `user` varchar(45) NOT NULL,
-  `proyect_title` varchar(45) DEFAULT NULL,
-  `proyect_description` varchar(200) DEFAULT NULL,
-  `proyect_start_date` date DEFAULT NULL,
-  `proyect_end_date` date DEFAULT NULL,
-  `proyect_photo` varchar(200) DEFAULT NULL,
-  `persona_id` int(11) NOT NULL,
-  `persona_user` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `skill`
---
-
-CREATE TABLE `skill` (
-  `id` int(11) NOT NULL,
-  `user` varchar(45) NOT NULL,
-  `skill_title` varchar(45) DEFAULT NULL,
-  `skill_tipe` varchar(45) DEFAULT NULL,
-  `skill_level` varchar(45) DEFAULT NULL,
-  `skill_description` varchar(150) DEFAULT NULL,
-  `persona_id` int(11) NOT NULL,
-  `persona_user` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `education`
---
-ALTER TABLE `education`
-  ADD PRIMARY KEY (`id`,`persona_id`,`persona_user`),
-  ADD KEY `fk_education_persona1` (`persona_id`,`persona_user`);
-
---
--- Indices de la tabla `job_experience`
---
-ALTER TABLE `job_experience`
-  ADD PRIMARY KEY (`id`,`persona_id`,`persona_user`,`job_type_id`),
-  ADD KEY `fk_job_experience_persona` (`persona_id`,`persona_user`),
-  ADD KEY `fk_job_experience_job_type1` (`job_type_id`);
-
---
--- Indices de la tabla `job_type`
---
-ALTER TABLE `job_type`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `persona`
---
-ALTER TABLE `persona`
-  ADD PRIMARY KEY (`id`,`user`);
-
---
--- Indices de la tabla `proyect`
---
-ALTER TABLE `proyect`
-  ADD PRIMARY KEY (`id`,`persona_id`,`persona_user`),
-  ADD KEY `fk_proyect_persona1` (`persona_id`,`persona_user`);
-
---
--- Indices de la tabla `skill`
---
-ALTER TABLE `skill`
-  ADD PRIMARY KEY (`id`,`persona_id`,`persona_user`),
-  ADD KEY `fk_skill_persona1` (`persona_id`,`persona_user`);
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `education`
---
-ALTER TABLE `education`
-  ADD CONSTRAINT `fk_education_persona1` FOREIGN KEY (`persona_id`,`persona_user`) REFERENCES `persona` (`id`, `user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `job_experience`
---
-ALTER TABLE `job_experience`
-  ADD CONSTRAINT `fk_job_experience_job_type1` FOREIGN KEY (`job_type_id`) REFERENCES `job_type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_job_experience_persona` FOREIGN KEY (`persona_id`,`persona_user`) REFERENCES `persona` (`id`, `user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `proyect`
---
-ALTER TABLE `proyect`
-  ADD CONSTRAINT `fk_proyect_persona1` FOREIGN KEY (`persona_id`,`persona_user`) REFERENCES `persona` (`id`, `user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `skill`
---
-ALTER TABLE `skill`
-  ADD CONSTRAINT `fk_skill_persona1` FOREIGN KEY (`persona_id`,`persona_user`) REFERENCES `persona` (`id`, `user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
